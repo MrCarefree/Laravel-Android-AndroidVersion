cde7bd164ef569a4ff84e605a9c43ce65cb9bf36
MrCarefree <fpf159624753>
2019-12-05 10:40:30 +0700

    add RegisterActivity and layout design

---
+++ app/src/main/java/com/mrcarefree/laravel_android/ui/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.mrcarefree.laravel_android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mrcarefree.laravel_android.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
